af2fa0d5a1cd06836812a949036fc854c9f2acde
danipabernales <dbernales.miranda@gmail.com>
2024-02-28 22:05:01 -0300

    Ejercicio clase 3

---
+++ ejercicio-clase-3/app.py
@@ -0,0 +1,11 @@
+def leer_archivo(nombre_archivo):
+    with open(nombre_archivo) as archivo:
+        return [i for i in archivo.read().split("\n")]
+
+lista = leer_archivo('nombres.txt')
+print(f"Datos en el archivo: {lista}")
+
+lista_numeros = leer_archivo('numeros.txt')
+print("Archivo 2 (numeros.txt)")
+for numero in lista_numeros:
+    print(int(numero))
